6bbca993bd37c07ce9348f188a449c4a1d47c989
lennin <durangolennin@gmail.com>
2025-02-26 22:09:53 -0500

    Create typin-text.js

---
+++ assets/js/typin-text.js
@@ -0,0 +1,6 @@
+const stringsArray = ['Back-End', 'Product Owner'];
+const typed = new Typed('.typing-text', {
+            strings: stringsArray, 
+            typeSpeed: 120,         
+            loop: true, 
+});
